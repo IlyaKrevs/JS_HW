815ef32273ef0f257ac1338d3c6d22bcdea2b89d
Ilya <m-u-r-z-a@mail.ru>
2022-11-22 01:30:45 +0500

    task1 finish

---
+++ homework/3hw/script.js
@@ -0,0 +1,11 @@
+// // Task1
+// function CubeOf(arg1) {
+//     return arg1 ** 3;
+// }
+// console.log(CubeOf(2), CubeOf(3));
+
+// Task2
+let userNumber = prompt("Enter your salary");
+if (typeof userNumber !== typeof 0) {
+    console.log("Wrong number");
+}
